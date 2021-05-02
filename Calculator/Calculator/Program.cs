b487a12f75ef9f20f5ea56f32269f35ff709e2f4
DESKTOP-DQLTBLK\janna <j.amhausend@gmail.com>
2021-05-02 23:08:16 +0200

    addet calulator with basic functions

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator(23, 4);
+            calculator.Add();
+            calculator.Sub();
+        }
+    }
+}
---
+++ Calculator/Calculator/calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Number1 { private get; set; }
+
+        public int Number2 { private get; set; } 
+
+        public Calculator(int number1, int number2)
+        {
+            Number1 = number1;
+            Number2 = number2;
+        }
+
+        public void Add()
+        {
+            Console.WriteLine($"{Number1} + {Number2} = {Number1 + Number2}");
+        }
+
+        public void Sub()
+        {
+            Console.WriteLine($"{Number1} - {Number2} = {Number1 - Number2}");
+        }
+    }
+}
