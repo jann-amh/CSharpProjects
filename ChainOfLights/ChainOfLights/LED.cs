2e29b25dfc1d5346589e2fc4e7436276ef03b57b
LAPTOP-4E5S1PBM\janna <j.amhausend@gmail.com>
2021-11-09 09:17:29 +0100

    new list exercises

---
+++ ChainOfLights/ChainOfLights/LED.cs
@@ -0,0 +1,38 @@
+﻿namespace ChainOfLights
+{
+    public class LED
+    {
+        public bool Power { get; set; }
+
+        public string Name { get; set; }
+
+        public void onPower()
+        {
+            Power = true;
+        }
+
+        public void offPower()
+        {
+            Power = false;
+        }
+
+        public override string ToString()
+        {
+            return "Name" + Name + "Power" + Power;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            LED objAsPart = obj as LED;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+
+        public bool Equals(LED other)
+        {
+            if (other == null) return false;
+            return (this.Name.Equals(other.Name));
+        }
+    }
+}
---
+++ ChainOfLights/ChainOfLights/Program.cs
@@ -0,0 +1,19 @@
+﻿using ChainOfLights;
+
+namespace chainoflights;
+public class Program
+{
+    private static void Main()
+    {
+        List<LED> lEDs = new List<LED>();
+
+        lEDs.Add(new LED() { Power = true, Name = "led01" });
+        lEDs.Add(new LED() { Power = true, Name = "led02" });
+        lEDs.Add(new LED() { Power = true, Name = "led03" });
+
+        foreach (LED led in lEDs)
+        {
+            Console.WriteLine(led);
+        }
+    }
+}
