270e876fcf5aa251a816b3a1c63cc38070689902
jann_amh <j.amhausend@gmail.com>
2022-02-22 18:56:44 +0100

    ASCIIConverter

---
+++ ASCIIConverter/ASCIIConverter/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCIIConverter
+{
+    public class Converter
+    {
+        public char GetChar(int i)
+        {
+            return (char)i;
+        }
+
+        public int GetInt(char c)
+        {
+            return c;
+        }
+    }
+}
---
+++ ASCIIConverter/ASCIIConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ASCIIConverter;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        Converter converter = new();
+
+        string s = Console.ReadLine();
+
+        char c = converter.GetChar(Convert.ToInt32((s)));
+
+        Console.WriteLine(c);
+
+    }
+}
